aaa376245758ddd35a50c9d023287fb2b2e9b239
shcoder <32806797+shanhaicoder@users.noreply.github.com>
2023-11-19 15:42:07 +0800

    Update miner.py
    new blue code 34 to 36

--- miner.py
+++ miner.py
@@ -277,7 +277,7 @@ def submit_pow(account_address, key, hash_to_verify):
 RED = "\033[31m"
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
-BLUE = "\033[34m"
+BLUE = "\033[36m"
 RESET = "\033[0m"
 
 def mine_block(stored_targets, prev_hash, address):
